c0583b21c0fc1af3344802d5d997b2809f889fc1
Jacob Truman <jacob.truman@gmail.com>
2026-03-09 13:57:04 -0600

    Update tech research requirement

--- prototypes/space-location.lua
+++ prototypes/space-location.lua
@@ -42,7 +42,7 @@ data:extend {
       },
     },
     unit = {
-      count = 200,
+	      count = 2000,
       ingredients = {
         { "automation-science-pack", 1 },
         { "logistic-science-pack", 1 },
